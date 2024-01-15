9fcf4d4203dc935c90a7b4b799d2a6ee41e595fe
BadalPatel17 <124709920+BadalPatel17@users.noreply.github.com>
2024-01-15 13:30:30 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,47 @@
+//Control how fast game goes
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
+} from "./snake.js";
+
+import { update as updateFood, draw as drawFood } from "./food.js";
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementbyId("game-board");
+
+function main(currentTime) {
+  if (gameOver) {
+    return alert("you lose");
+  }
+
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+  updateFood();
+  checkDeath();
+}
+
+function draw() {
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
